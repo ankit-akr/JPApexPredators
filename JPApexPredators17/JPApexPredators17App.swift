b6d675f7b22bcc5d65d45836aa7b6a08b6c9ef28
Ankit Kumar <ankitku@microsoft.com>
2024-06-01 15:29:50 +0530

    JPApexPredators App

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Ankit Kumar on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
